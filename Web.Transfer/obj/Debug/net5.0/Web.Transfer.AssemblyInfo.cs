a18819a3e4771b99b06a50ea9be86720fe0faf21
Giovane Junior <pj_giovanejunior@bocombbm.com.br>
2022-09-01 16:58:51 -0300

    add docker

--- Web.Transfer/obj/Debug/net5.0/Web.Transfer.AssemblyInfo.cs
+++ Web.Transfer/obj/Debug/net5.0/Web.Transfer.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("7054d5a7-3245-45c4-9bd4-7da8eb8913d5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.Transfer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
